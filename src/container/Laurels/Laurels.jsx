e5b50b02d55efc9d98d00a1303e3ee5bcf01cc99
GabrielSB19 <gasuarez2002@gmail.com>
2023-08-21 21:50:18 -0500

    Doing the view of the gallery

---
+++ src/container/Laurels/Laurels.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import { images, data } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Laurels.css";
+
+const AwardCard = ({ imgUrl, title, subtitle }) => {
+  return (
+    <div className="app__laures_awards-card">
+      <img src={imgUrl} alt="award" />
+      <div className="app__laurels_award-card_content">
+        <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+          {title}
+        </p>
+        <p className="p__cormorant">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+const Laurels = () => (
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title={"Awards & recognition"} />
+      <h1 className="headtext__cormorant">Our Laures</h1>
+      <div className="app__laures_awards">
+        {data.awards.map((award, index) => (
+          <AwardCard
+            key={index}
+            imgUrl={award.imgUrl}
+            title={award.title}
+            subtitle={award.subtitle}
+          />
+        ))}
+      </div>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.laurels} alt="laurels" />
+    </div>
+  </div>
+);
+
+export default Laurels;
